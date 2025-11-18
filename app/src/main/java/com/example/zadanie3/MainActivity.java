85085551996447c9044fa02649aed1e0c950e00b
StanislawMachnik <stanislaw.machnik@uczen.mechaniktg.pl>
2025-11-18 20:09:51 +0100

    Kolorowanie bledow i potwierdzen

--- app/src/main/java/com/example/zadanie3/MainActivity.java
+++ app/src/main/java/com/example/zadanie3/MainActivity.java
@@ -5,13 +5,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText pass2;
     private Button submit;
     private TextView message;
+    private TextView errorBox;
+    private TextView errorBoxChars;
+    private TextView errorBoxNumbers;
+    private TextView errorBoxLowercase;
+    private TextView errorBoxUppercase;
+    private TextView strength;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,28 +35,37 @@ public class MainActivity extends AppCompatActivity {
         pass2 = findViewById(R.id.pass2);
         submit = findViewById(R.id.submit);
         message = findViewById(R.id.message);
+        errorBox = findViewById(R.id.errorBox);
+        errorBoxChars = findViewById(R.id.errorBoxChars);
+        errorBoxNumbers = findViewById(R.id.errorBoxNumbers);
+        errorBoxLowercase = findViewById(R.id.errorBoxLowercase);
+        errorBoxUppercase = findViewById(R.id.errorBoxUppercase);
+        strength = findViewById(R.id.strength);
 
         submit.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        String welcomeMessage = "";
+                        String errorMessage = "";
+                        String strengthMessage = "";
+                        String addToPass = "";
+                        int passStrength = 0;
+
                         String checkEmail = String.valueOf(email.getText());
                         String checkPass1 = String.valueOf(pass1.getText());
                         String checkPass2 = String.valueOf(pass2.getText());
 
-                        int passStrength = 0;
                         boolean charsSpecial = false;
                         boolean charsNumber = false;
                         boolean charsUppercase = false;
                         boolean charsLowercase = false;
-                        String error = "";
-                        String addToPass = "";
 
                         if(!checkEmail.contains("@")){
-                            error = "Niepoprawny adres e-mail";
+                            errorMessage = "Niepoprawny adres e-mail";
                         }
                         else if (!checkPass1.equals(checkPass2)) {
-                            error = "Hasła się różnią";
+                            errorMessage = "Hasła się różnią";
                         }
                         else{
                             String chars = "!@#$%^&*()_+-=?/.,<>{}|[]";
@@ -61,52 +74,74 @@ public class MainActivity extends AppCompatActivity {
                             String lowercase = "qwertyuiopasdfghjklzxcvbnm";
 
                             for (int i = 0; i < checkPass1.length(); i++){
-                                if(chars.contains(checkPass1.charAt(i)+"")){
+                                String password = checkPass1.charAt(i)+"";
+
+                                if(chars.contains(password)){
                                     charsSpecial = true;
                                 }
 
-                                if(numbers.contains(checkPass1.charAt(i)+"")){
+                                if(numbers.contains(password)){
                                     charsNumber = true;
                                 }
 
-                                if(uppercase.contains(checkPass1.charAt(i)+"")){
+                                if(uppercase.contains(password)){
                                     charsUppercase = true;
                                 }
 
-                                if(lowercase.contains(checkPass1.charAt(i)+"")){
+                                if(lowercase.contains(password)){
                                     charsLowercase = true;
                                 }
                             }
+                            if(charsSpecial){
+                                passStrength++;
+                                errorBoxChars.setText("Są znaki specjalne!");
+                                errorBoxChars.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_green_light));
+                            }
+                            else{
+                                errorBoxChars.setText("Brak znaku specjalnego!");
+                                errorBoxChars.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_light));
+                            }
+
+                            if(charsNumber){
+                                passStrength++;
+                                errorBoxNumbers.setText("Są liczby!");
+                                errorBoxNumbers.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_green_light));
+                            }
+                            else{
+                                errorBoxNumbers.setText("Brak liczby!");
+                                errorBoxNumbers.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_light));
+                            }
+
+                            if(charsLowercase){
+                                passStrength++;
+                                errorBoxLowercase.setText("Są małe litery!");
+                                errorBoxLowercase.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_green_light));;
+                            }
+                            else{
+                                errorBoxLowercase.setText("Brak małej litery!");
+                                errorBoxLowercase.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_light));
+                            }
+
+                            if(charsUppercase){
+                                passStrength++;
+                                errorBoxUppercase.setText("Są duże litery!");
+                                errorBoxUppercase.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_green_light));
+                            }
+                            else{
+                                errorBoxUppercase.setText("Brak dużej litery!");
+                                errorBoxUppercase.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_light));
+                            }
+
                             if(checkPass1.length() >= 10){
                                 passStrength++;
                             }
-                            if(charsSpecial){
-                                passStrength++;
-                            }
-                            if(charsNumber){
-                                passStrength++;
-                            }
-                            if(charsLowercase){
-                                passStrength++;
-                            }
-                            if(charsUppercase){
-                                passStrength++;
-                            }
-                            if(!charsUppercase){
-                                addToPass += "\nBrak dużej litery";
-                            }
-                            if(!charsLowercase){
-                                addToPass += "\nBrak małej litery";
-                            }
-                            if(!charsNumber){
-                                addToPass += "\nBrak liczby";
-                            }
-                            if(!charsSpecial){
-                                addToPass += "\nBrak znaku specjalnego";
-                            }
-                            error = "Witaj " + checkEmail + "\n Siła hasła = " + passStrength + addToPass;
+
+                            strengthMessage = "Siła: " + passStrength;
+                            welcomeMessage = "Witaj " + checkEmail + "!";
                         }
-                        message.setText(error);
+                        message.setText(welcomeMessage);
+                        errorBox.setText(errorMessage);
+                        strength.setText(strengthMessage);
                     }
                 }
         );
